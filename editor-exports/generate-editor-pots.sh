1a5b2cb854c475780a224f281a0ac64644336f10
Chris Willis-Ford <cwillisf@media.mit.edu>
2016-01-28 17:06:44 +0000

    Tweak {blocks,editor}.pot gen for cleaner diffs
    These files are now sorted and no longer contain file location
    indicators. If we upgrade to newer gettext tools we should consider
    using "--add-location=file"

--- editor-exports/generate-editor-pots.sh
+++ editor-exports/generate-editor-pots.sh
@@ -3,9 +3,9 @@ set -e
 mkdir -p work
 for FILE in commands-online commands-offline uiStrings-online uiStrings-offline; do
 	sort -u < ${FILE}.txt | sed '/^$/d;G' > work/${FILE}.txt
-	(cd work && txt2po --pot -i ${FILE}.txt -o ${FILE}.pot)
+	(cd work && txt2po --pot -i ${FILE}.txt | msgattrib --no-location -o ${FILE}.pot)
 done
-msgcat work/commands-online.pot work/commands-offline.pot -o ../blocks/templates/blocks.pot
-msgcat work/uiStrings-online.pot work/uiStrings-offline.pot -o ../editor/templates/editor.pot
+msgcat --sort-output work/commands-online.pot work/commands-offline.pot -o ../blocks/templates/blocks.pot
+msgcat --sort-output work/uiStrings-online.pot work/uiStrings-offline.pot -o ../editor/templates/editor.pot
 msgfmt --check-format --check-domain ../blocks/templates/blocks.pot -o /dev/null
 msgfmt --check-format --check-domain ../editor/templates/editor.pot -o /dev/null
